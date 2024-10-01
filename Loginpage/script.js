0fc8456b4ee9ca87943ea3626a656bab7c0dbf50
YAMINISARASWATHI04 <yaminisaraswathi72@gmail.com>
2024-10-01 23:23:09 +0530

    Change Loginpage

--- Loginpage/script.js
+++ Loginpage/script.js
@@ -1,26 +1,23 @@
 // script.js
 
-// script.js
 
 document
   .getElementById("loginForm")
   .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting automatically
+
     // Get form values
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
     // Simple validation
-    var passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-
-    // Simple validation
     if (username === "" || password === "") {
       displayErrorMessage("Please fill in both fields.");
-    } else if (!passwordPattern.test(password)) {
-      displayErrorMessage(
-        "Password must be at least 8 characters, include one letter and one number."
-      );
     } else {
       displayErrorMessage("Login successful!", "success");
+
+      // Clear form data after successful login
+      document.getElementById("loginForm").reset();
     }
   });
 
