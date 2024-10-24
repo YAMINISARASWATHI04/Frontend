b74e8c5249173c28e7f94104ec82390d8efe8e0a
YAMINISARASWATHI04 <yaminisaraswathi72@gmail.com>
2024-10-24 11:46:07 +0530

    Time table

--- JWT/server2.js
+++ JWT/server2.js
@@ -6,11 +6,15 @@ app.use(express.json())
 require('dotenv').config()
 const posts = [
     {
-        name: "CBIT",
+        id: "149",
+        name: "YaminiSaraswathi",
+        branch: "IT",
         title: "Welcome to CBIT"
     },
     {
-        name: "MGIT",
+        id: "100",
+        name: "ISHU",
+        branch: "CSE",
         title: "Welcome to MGIT"
     }
 ]
@@ -29,8 +33,8 @@ const authenticateToken = (req, res, next) => {
     })
 }
 app.post('/login', (req, res) => {
-    const username = req.body.username
-    const user = { name: username }
+    const userid = req.body.userid
+    const user = { id: userid }
 
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN)
 
@@ -38,8 +42,8 @@ app.post('/login', (req, res) => {
 })
 app.use(authenticateToken)
 app.get('/posts', (req, res) => {
-    console.log(req.user.name)
-    res.json(posts.filter(post => post.name === req.user.name))
+    console.log(req.user.id)
+    res.json(posts.filter(post => post.id === req.user.id))
 
 })
 app.listen(4000)
